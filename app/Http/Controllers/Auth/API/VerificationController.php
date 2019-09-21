0ec695f9d4e9b80fd8abd32308fe1431e4a32293
Ibrahim Samad <naatogma@gmail.com>
2019-09-21 18:05:08 +0000

    Implement response for invalid signature

--- app/Http/Controllers/Auth/API/VerificationController.php
+++ app/Http/Controllers/Auth/API/VerificationController.php
@@ -38,7 +38,7 @@ class VerificationController extends Controller
     public function __construct()
     {
         $this->middleware('auth:api')->only('resend');
-        $this->middleware('signed')->only('verify');
+        $this->middleware('signed.api')->only('verify');
         $this->middleware('throttle:6,1')->only('verify', 'resend');
     }
 
